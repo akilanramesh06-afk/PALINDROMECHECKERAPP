6aa5b720ef8db7cb0208f8858adc0dfd1c61fa92
AkilanRamesh <ar2059@srmist.edu.in>
2026-03-15 16:43:30 +0530

    AK Added UC11

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -18,6 +18,25 @@
      */
 import java.util.Scanner;
 
+class PalindromeChecker {
+
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
+
 class PalindromeCheckerApp {
 
     public static void main(String[] args) {
@@ -26,19 +45,11 @@ class PalindromeCheckerApp {
         System.out.print("Input : ");
         String input = sc.nextLine();
 
-        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+        PalindromeChecker checker = new PalindromeChecker();
 
-        boolean isPalindrome = true;
-        int n = normalized.length();
+        boolean result = checker.checkPalindrome(input);
 
-        for (int i = 0; i < n / 2; i++) {
-            if (normalized.charAt(i) != normalized.charAt(n - i - 1)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Is Palindrome? : " + result);
 
         sc.close();
     }
